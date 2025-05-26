c181c62c0e39db2b61704f2a05ff1f3236cabeb4
Kevin Yankam <yankamkevin95@gmail.com>
2025-05-26 15:03:33 +0200

    Infinite Contacts List : (gradle) fix gradle version issues.

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
